0b37d43bbdebb89259bd4d2ad1e444463b23b1d1
Apfe1Saft <foraaivanov@gmail.com>
2022-07-02 18:18:51 +0300

    create all

---
+++ src/main/java/ru/yandex/practicum/filmorate/exception/NoSuchReviewException.java
@@ -0,0 +1,5 @@
+package ru.yandex.practicum.filmorate.exception;
+
+public class NoSuchReviewException extends RuntimeException{
+    public NoSuchReviewException(String message){super(message);}
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -0,0 +1,30 @@
+package ru.yandex.practicum.filmorate.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.Optional;
+
+@Data
+@AllArgsConstructor
+public class Review {
+    @NotNull
+    int id;
+    @NotNull
+    @NotBlank
+    String content;
+    @NotNull
+    int userId;
+    @NotNull
+    int filmId;
+    @NotNull
+    int useful;
+    @NotNull Optional<Boolean> isPositive;
+    @JsonProperty(value="isPositive")
+    public boolean isPositive() {
+        return isPositive.get();
+    }
+}
